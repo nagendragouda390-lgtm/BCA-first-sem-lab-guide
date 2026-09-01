a6108288d12b22b68ed3dca997abe495fb3c0a76
Nagendra Gouda <nagendragouda390@gmail.com>
2026-09-01 15:35:46 +0530

    Declare character variable and update comments
    Added a character variable and comments for clarity.

--- Basics/Variables/code.c
+++ Basics/Variables/code.c
@@ -7,3 +7,15 @@ void main()
   int b = 20; //Initialisation of variable
   float c = 5.63;
   double d = 4.24323223; // For big decimals
+  char e = 'A'; // To store single character
+  /*
+  Displaying variables
+  Note:
+     \n - new line
+     %d - integer
+     %f - float
+     %lf - double
+     %c - character
+  */
+  printf("%d \n",a);
+}
