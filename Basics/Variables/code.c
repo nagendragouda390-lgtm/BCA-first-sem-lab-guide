0466544f8dafee34da00d7799c89e662e8b34e05
Nagendra Gouda <nagendragouda390@gmail.com>
2026-09-01 15:30:40 +0530

    Add variable declarations in code.c
    Added variable declarations and initializations for different data types.

---
+++ Basics/Variables/code.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <conio.h>
+
+void main()
+{
+  int a; // Daclaring variable of type int
+  int b = 20; //Initialisation of variable
+  float c = 5.63;
+  double d = 4.24323223; // For big decimals
