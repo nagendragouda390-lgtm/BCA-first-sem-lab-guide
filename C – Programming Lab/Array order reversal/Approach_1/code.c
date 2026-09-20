b1a1cdee8e243940da9febd58a9a88386cdc3f7b
Nagendra Gouda <nagendragouda390@gmail.com>
2026-09-20 12:59:04 +0530

    Implement array reversal in code.c
    Added functionality to reverse and display an array.

---
+++ C – Programming Lab/Array order reversal/Approach_1/code.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <conio.h>
+
+void main()
+{
+    int nums[100],n,i;
+
+    clrscr();
+
+    printf("No of elements : ");
+    scanf("%d",&n);
+
+    for(i=0; i < n ; i++)
+    {
+        printf("Element %d : ",i+1);
+        scanf("%d",&nums[i]);
+    }
+  
+    printf("Reversed arrray : ");
+    for(i=n-1; i >= 0; i++)
+    {
+        printf("%d\t",nums[i]);
+    }
+
+    getch();
+}
