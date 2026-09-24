73317ee8ceec69ae0f295cf4416e4752ddea59e0
Nagendra Gouda <nagendragouda390@gmail.com>
2026-09-24 12:34:19 +0530

    Add code_2.c to display ASCII code of a character

---
+++ Algorithm Lab/ASCII_code/code_2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <conio.h>
+
+void main()
+{
+  char ch;
+
+  clrscr();
+
+  printf("Enter a character : ");
+  scanf("%c",&ch);
+
+  printf("ASCII code of %c is %d.",ch,ch);
+
+  getch();
+}
